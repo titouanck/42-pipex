d9a14364522c746d56bf6a1ef3dff10f975a179a
titouan_ck <chevrier.titouan@gmail.com>
2023-01-26 01:45:46 +0100

    perror on open_files()

--- srcs/open_files.c
+++ srcs/open_files.c
@@ -6,7 +6,7 @@
 /*   By: tchevrie <tchevrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 01:57:46 by tchevrie          #+#    #+#             */
-/*   Updated: 2023/01/26 01:26:23 by tchevrie         ###   ########.fr       */
+/*   Updated: 2023/01/26 01:38:55 by tchevrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	open_infile(const char *filename)
 	int	fd;
 
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("pipex: ", 2);
+		perror(filename);
+	}
 	return (fd);
 }
 
@@ -25,6 +30,11 @@ int	open_outfile(const char *filename)
 	int	fd;
 
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if (fd == -1)
+	{
+		ft_putstr_fd("pipex: ", 2);
+		perror(filename);
+	}
 	return (fd);
 }
 
@@ -33,5 +43,10 @@ int	here_doc_open_outfile(const char *filename)
 	int	fd;
 
 	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	if (fd == -1)
+	{
+		ft_putstr_fd("pipex: ", 2);
+		perror(filename);
+	}
 	return (fd);
 }
